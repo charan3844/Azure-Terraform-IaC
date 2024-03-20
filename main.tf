3f0245876d574c098b2decf7dde91b7f7f5d99ae
chaRAN <81507077+charan3844@users.noreply.github.com>
2024-03-20 20:31:09 +0530

    abc

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
    client_id       = "2d673968-6dc2-4ad9-90bb-34b5350ebde9"  
    client_secret   = "LSD8Q~noOZhEiEO~epgqNLeSJVnXgjawuqAYZcau"
    tenant_id       = "a87c0125-feb0-4b78-9e50-9fa993143c7b"  
-   subscription_id = "a9985999-6feb-45f7-a0dd-3b477b9e9632"
+   
 }
 
 #Resorce Block
