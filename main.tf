e4ef0fa0be0e9ccfc391f48d7432dfc4cd854be3
charan3844 <saicharan.devara@citrix.com>
2024-03-21 16:02:34 +0530

    new-4

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ terraform {
 provider "azurerm" {
   features {} 
  
-  Devara Saicharan
+
   client_id       = "2d673968-6dc2-4ad9-90bb-34b5350ebde9"
   client_secret   = "LSD8Q~noOZhEiEO~epgqNLeSJVnXgjawuqAYZcau"
   client_secret   = "LSD8Q~noOZhEiEO~epgqNLeSJVnXgjawuqAYZcau"
