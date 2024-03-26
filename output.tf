53b1f20f9f0b60bb0dcc5c951a60fbb0540cb051
charan3844 <saicharan.devara@citrix.com>
2024-03-26 14:04:59 +0530

    Cherry-pick-4

--- output.tf
+++ output.tf
@@ -44,3 +44,8 @@ output "password" {
   description = "Output is the sensitive data"
 }
 
+output "not sensitive" {
+  value     = azurerm_windows_virtual_machine.VM.admin_password
+  description = "Output is the sensitive data"
+}
+
