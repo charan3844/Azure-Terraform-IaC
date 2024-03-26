a59b611883add60f154b181e08e2c86d46121c66
charan3844 <saicharan.devara@citrix.com>
2024-03-26 16:09:01 +0530

    git cherry pick-2

--- output.tf
+++ output.tf
@@ -10,10 +10,6 @@ output "vnetfunction" {
   value = join("",[var.prefix], [var.vnetname])
 }
 
-output "publicip" {
-  value = azurerm_windows_virtual_machine.VM.public_ip_address
-}
-
 output "publicipid" {
   value = azurerm_windows_virtual_machine.VM.id
 }
