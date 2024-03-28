c9a073fcce838046871bf42e233f7cb24ef2aad2
charan3844 <saicharan.devara@citrix.com>
2024-03-28 22:58:38 +0530

    empty

--- backend.tf
+++
@@ -1,9 +0,0 @@
-# Terraform block -1000
-terraform {
-backend "azurerm" {
-      resource_group_name  = "RG"
-      storage_account_name = "stoargeazurepipeline"
-      container_name       = "azurecontainer"
-      key                  = "remote.tfstate" #.tfstate give same
-  }
-}
